3ca6605bd14ac6c4f6c3f66c1456ab343e2bc825
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-08-08 20:34:23 +0800

    Update audio-speech.js

--- api/audio-speech.js
+++ api/audio-speech.js
@@ -2,6 +2,11 @@ const { handleAudioSpeech } = require('../lib/token-utils');
 const apiSelector = require('../lib/api-selector');
 
 module.exports = async (req, res) => {
-  await apiSelector.loadResponseTimes();
-  await handleAudioSpeech(req, res);
+  try {
+    await apiSelector.loadResponseTimes();
+    await handleAudioSpeech(req, res);
+  } catch (error) {
+    console.error('Unhandled error in audio-speech:', error);
+    res.status(500).send(`Internal Server Error: ${error.message}`);
+  }
 };
