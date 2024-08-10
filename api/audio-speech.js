55c1348d716a8c10fd590ad3753e7dda78642f02
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-08-10 13:19:23 +0800

    Update audio-speech.js

--- api/audio-speech.js
+++ api/audio-speech.js
@@ -4,6 +4,8 @@ const apiSelector = require('../lib/api-selector');
 module.exports = async (req, res) => {
   try {
     await apiSelector.loadResponseTimes();
+    const apiUrl = await apiSelector.selectAPI();
+    console.log(`Using API URL for audio speech: ${apiUrl}`);
     await handleAudioSpeech(req, res);
   } catch (error) {
     console.error('Unhandled error in audio-speech:', error);
